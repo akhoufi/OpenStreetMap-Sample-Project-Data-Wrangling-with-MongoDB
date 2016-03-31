74a7f6bb08cd109d52037d68a06c1cda29617a4b
Asma Khoufi <a.khoufi@gmail.com>
2016-03-31 10:49:50 +0200

    - Fogoetten commit

--- AuditingMap.py
+++ AuditingMap.py
@@ -7,34 +7,93 @@ Created on Mon Mar 21 12:00:06 2016
 
 import xml.etree.cElementTree as ET
 
-filename='tunis_tunisia.osm'
 
 #find what 'k' tags exists on the map
-#k_set=set([])
-#for _,element in ET.iterparse(filename):
-#    if element.tag=='tag':
-#        for tag in element.iter('tag'):
-#            if tag.attrib['k'] not in k_set:
-#                k_set.add(tag.attrib['k'])
-#                print  tag.attrib['k']      
+def list_tags(filename):
+    tag_set=set()
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if tag.attrib['k'] not in tag_set:
+                    tag_set.add(tag.attrib['k'])
+                    print  tag.attrib['k']    
+    return tag_set
 
 #check if all the way tag name are in the same language
-#for _,element in ET.iterparse(filename):
-#    if element.tag=='tag':
-#        for tag in element.iter('tag'):
-#            if tag.attrib['k']=='name':
-#                print tag.attrib['v']
+def list_tag_names(filename):
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if tag.attrib['k']=='name':
+                    print tag.attrib['v']
+
                 
 #check phone numbers and fax numbers different formats
-for _,element in ET.iterparse(filename):
-    if element.tag=='tag':
+def list_phone_numbers(filename):
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if 'phone' in tag.attrib['k'] or 'fax' in tag.attrib['k'] or 'tel' in tag.attrib['k']:
+                    print tag.attrib['v']
+                    
+
+               
+#check emails different formats
+def list_emails(filename):
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if 'email' in tag.attrib['k']:
+                    print tag.attrib['v']
+                
+#check address housenumber for anything different than numbers
+def list_housenumbers(filename):
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if 'addr:housenumber' in tag.attrib['k']:
+                    print tag.attrib['v']
+
+#Check for postal codes formats
+def list_postcodes(filename):
+    for _,element in ET.iterparse(filename):
+        if element.tag=='tag':
+            for tag in element.iter('tag'):
+                if 'addr:postcode' in tag.attrib['k']:
+                    print tag.attrib['v']
+            
+#check if number of governorate is right
+def count_governorate(filename):
+    governorate_set=set([])
+    for _,element in ET.iterparse(filename):
         for tag in element.iter('tag'):
-            if 'phone' in tag.attrib['k'] or 'fax' in tag.attrib['k'] or 'tel' in tag.attrib['k']:
-                print tag.attrib['v']
-#                
-##check emails different formats
-#for _,element in ET.iterparse(filename):
-#    if element.tag=='tag':
-#        for tag in element.iter('tag'):
-#            if 'email' in tag.attrib['k']:
-#                print tag.attrib['v']
+            if 'governorate' in tag.attrib['k'] and tag.attrib['v'] not in governorate_set:
+                governorate_set.add(tag.attrib['v'])
+    return governorate_set              
+                
+def list_country(filename):
+    governorate_set=set([])
+    for _,element in ET.iterparse(filename):
+        for tag in element.iter('tag'):
+            if 'governorate' in tag.attrib['k'] and tag.attrib['v'] not in governorate_set:
+                governorate_set.add(tag.attrib['v'])
+    return governorate_set   
+def audit():
+    filename='tunis_tunisia.osm'
+#    list_tags(filename)
+#    print('#################################################')
+#    list_tag_names(filename)
+#    print('#################################################')
+#    list_phone_numbers(filename)
+#    print('#################################################')
+#    list_emails(filename)
+#    print('#################################################')
+#    list_housenumbers(filename)
+#    print('#################################################')
+#    list_postcodes(filename)
+#    print('#################################################')
+    governorate=count_governorate(filename)
+    print 'Number of governorates',len(governorate)
+    print governorate
+audit()
+    
---
+++ Cleaning.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 14:33:47 2016
+
+@author: akhou
+"""
+
+clean()
